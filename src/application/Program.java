c1504b9d312e15fba61c9f0566581449ece57a03
Mônica Mallmann <monica-mallmann@hotmail.com>
2022-01-29 14:21:56 -0300

    Posição

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,24 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import jogodetabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Olar galera");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		Posicao pos = new Posicao(3,5);
+		System.out.println(pos);
+		
+
+		
+		
+		
+		sc.close();
 	}
 
 }
---
+++ src/jogodetabuleiro/Posicao.java
@@ -0,0 +1,27 @@
+package jogodetabuleiro;
+
+public class Posicao {
+
+	private Integer linha;
+	private Integer coluna;
+	
+	public Posicao() {
+	}
+	public Posicao(Integer linha, Integer coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	public Integer getLinha() {
+		return linha;
+	}
+	public void setLinha(Integer linha) {
+		this.linha = linha;
+	}
+	public void getColuna() {
+		this.coluna = coluna;
+	}
+	@Override
+	public String toString() {
+		return +linha+", "+coluna;
+	}
+}
