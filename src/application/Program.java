0c10a2525da5f8f6e3d2b7f4a25cd513b305de96
Mônica Mallmann <monica-mallmann@hotmail.com>
2022-01-29 14:54:02 -0300

    Começando a implementar Peca e Tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import jogodetabuleiro.Posicao;
+import jogodetabuleiro.Tabuleiro;
 
 public class Program {
 
@@ -11,9 +11,7 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		Posicao pos = new Posicao(3,5);
-		System.out.println(pos);
-		
+		Tabuleiro tabuleiro = new Tabuleiro(8,5);
 
 		
 		
